0ff5605efceed14d15aa8b0ac3773f418a916e32
nay778 <naynaingw@gmail.com>
2022-01-18 15:35:37 +0630

    [features/userEdit] user edit uploaded

--- HealthIsWealthProject/resources/views/customer/user_edit.blade.php
+++ HealthIsWealthProject/resources/views/customer/user_edit.blade.php
@@ -2,49 +2,48 @@
 @section('content')
 <div class="adduser">
     <div class="cardHeader">Edit User Role</div>
-    <form method="POST" enctype="multipart/form-data" action="{{ route('user_role_update',$user->id) }}">
+    <form method="POST" action="{{ route('user_role_update',$user->id) }}" enctype="multipart/form-data">
         @csrf
         <div class="form-group">
-            <input type="text" placeholder="Name" class="form-control" name="name" id="name" value="{{ $user->name }}" disabled>
+            <label for="name">Name : </label>
+            <input type="text" class="form-control" name="name" id="name" value="{{ $user->name }}" disabled>
             @if ($errors->has('name'))
             <span class="text-danger">{{ $errors->first('name') }}</span>
             @endif
         </div>
         <div class="form-group">
-
-            <input type="email" placeholder="Email" class="form-control" value="{{ $user->email }}" name="email" id="email" disabled>
+            <label for="email">Email :</label>
+            <input type="email" class="form-control" value="{{ $user->email }}" name="email" id="email" disabled>
             @if ($errors->has('email'))
             <span class="text-danger">{{ $errors->first('email') }}</span>
             @endif
         </div>
         <div class="form-group">
-            (<small class="text-danger">* jpeg|png|gif|jpg</small>)
-            <input type="file" placeholder="User Profile" class="form-control" value="{{ old('profile') }}" name="profile" id="profile" accept="image/png, image/gif, image/jpeg">
-            @if ($errors->has('profile'))
-            <span class="text-danger">{{ $errors->first('profile') }}</span>
+            <label for="email">Certificate :</label>
+            @if($user->certificate)
+                <input type="checkbox" checked disabled>
             @endif
-        </div>
-        <label for="text" class="form-label">*option(if you have certificate)</label>
-
-        <div class="form-group">
-            <input type="file" placeholder="Certificate" class="form-control" id="certificate" name="certificate">
+               
         </div>
         <div class="form-group">
+            <label for="date"> Date Of Birth :</label>
             <input type="date" placeholder="Date Of Birth" class="form-control" id="dob" name="dob" value="{{ $user->dob }}" disabled>
             @if ($errors->has('dob'))
             <span class="text-danger">{{ $errors->first('dob') }}</span>
             @endif
         </div>
         <div class="form-group">
-            <input type="address" placeholder="Address" class="form-control" id="address" name="address" value="{{ $user->address }}" disabled>
+            <label for="address"> Address :</label>
+            <input type="address" class="form-control" id="address" name="address" value="{{ $user->address }}" disabled>
             @if ($errors->has('address'))
             <span class="text-danger">{{ $errors->first('address') }}</span>
             @endif
         </div>
         <div class="form-group">
-            <select name="type" id="type"  class="form-control">
-                <option value="user">USER</option>
-                <option value="admin">ADMIN</option>
+            <select name="type" id="type" class="form-control">
+                <option value="">Admin</option>
+                <option value="">Sub_admin</option>
+                <option value="">user</option>
             </select>
         </div>
         <button type="submit" class="btn btn-primary">Update</button>
