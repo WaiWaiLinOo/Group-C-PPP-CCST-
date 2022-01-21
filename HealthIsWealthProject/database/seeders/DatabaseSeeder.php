739dbb5a990ee1edb0bca0a6292024d5ed686022
Acer <yunthiri1998@gmail.com>
2022-01-21 11:01:04 +0630

    [features/post]seeder changes

--- HealthIsWealthProject/database/seeders/DatabaseSeeder.php
+++ HealthIsWealthProject/database/seeders/DatabaseSeeder.php
@@ -15,8 +15,9 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
         $this->call([
-            CreateAdminUserSeeder::class,
             PermissionTableSeeder::class,
+            CreateAdminUserSeeder::class,
+
         ]);
     }
 }
