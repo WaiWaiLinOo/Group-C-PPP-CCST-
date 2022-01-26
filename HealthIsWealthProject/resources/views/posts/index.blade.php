8e190a5fc5f11edf45da2fc098a8e9fafc30944e
wailinoo <wailinoo.august@gmail.com>
2022-01-26 20:39:42 +0630

    [features/category] uploaded

--- HealthIsWealthProject/resources/views/posts/index.blade.php
+++ HealthIsWealthProject/resources/views/posts/index.blade.php
@@ -98,14 +98,4 @@
 </div>
 
 {!! $posts->render() !!}
-<script>
-  // Get the modal
-  var modal = document.getElementById('id01');
-  // When the user clicks anywhere outside of the modal, close it
-  window.onclick = function(event) {
-    if (event.target == modal) {
-      modal.style.display = "none";
-    }
-  }
-</script>
 @endsection
