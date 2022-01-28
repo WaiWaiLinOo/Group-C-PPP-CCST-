0c69810ce2f56c394f4728b5eeee18e42a8ace70
Acer <yunthiri1998@gmail.com>
2022-01-28 17:34:41 +0630

    [features/adminView] post and categories modified

--- HealthIsWealthProject/resources/views/posts/edit.blade.php
+++ HealthIsWealthProject/resources/views/posts/edit.blade.php
@@ -1,6 +1,4 @@
-@extends('layouts.app')
-
-
+@extends('frontend.app')
 @section('content')
 @if (count($errors) > 0)
 <div class="alert alert-danger">
@@ -12,42 +10,41 @@
   </ul>
 </div>
 @endif
-<div class="adduser">
+<div class="register">
   <div class="cardHeader">Edit Post</div>
-  <form action="{{ route('posts.update',$post->id) }}" method="POST" enctype="multipart/form-data">
+  <form action="{{ route('posts.update',$post->id) }}" method="POST" enctype="multipart/form-data" class="register-form">
     @csrf
     @method('PUT')
-    <div class="register-form">
-
+    <div class="editform">
      <div class="form-group">
         <strong>Post Name:</strong>
         <input type="text" name="post_name" class="form-control" value="{{ $post->post_name }}" placeholder="Post name">
       </div>
-      <div class="form-group profile">
-        <label for="post_img">Post Image :</label>
-        <input type="file" class="form-control" name="post_img" id="post_img">
+      <div class="form-group mb-3 profile">
+        <label for="profile">Post Image</label>
+        (<small class="text-danger">*We only accept jpeg png gif jpg format</small>)
+        <input type="file" placeholder="User Profile" name="post_img" id="post_img" accept="image/png, image/gif, image/jpeg">
         <img src="{{ asset('post_img/' . $post->post_img) }}" class="post_img" />
       </div>
-      <!--test -->
-      <!-- Drop down -->
-      <label for="categories"><span>Choose a category:</span></label>
-      <select name="category_id" id="categories">
-        @foreach ($categories as $category)
-        @if (old('category') == $category->id || $category->id == $post->category_id)
-        <option value="{{ $category->id }}" selected>{{ $category->name }}</option>
-        @else
-        <option value="{{$category->id}}">{{ $category->name }}</option>
-        @endif
-        @endforeach
-      </select>
-      <!--test -->
+      <div class="form-group">
+        <label for="categories"><span>Choose a category:</span></label>
+        <select name="category_id" id="categories">
+          @foreach ($categories as $category)
+          @if (old('category') == $category->id || $category->id == $post->category_id)
+          <option value="{{ $category->id }}" selected>{{ $category->name }}</option>
+          @else
+          <option value="{{$category->id}}">{{ $category->name }}</option>
+          @endif
+          @endforeach
+        </select>
+      </div>
       <div class="form-group">
         <strong>Details:</strong>
         <textarea class="form-control" style="height:150px" name="detail" placeholder="Detail">{{ $post->detail }}</textarea>
       </div>
-      <button type="submit" class="button-secondary btns">Update</button>
+      <button type="submit" class="btns">Update</button>
       <div class="create-categories">
-        <a href="{{route('posts.index')}}">Post listx <span>&#8594;</span></a>
+        <a href="{{route('posts.index')}}"><i class="fas fa-arrow-circle-left"></i></a>
 
       </div>
     </div>
